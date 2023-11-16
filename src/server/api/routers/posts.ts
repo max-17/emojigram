5990ae5530e96d103b52b14aa2f7b8d2021f5c23
max-17 <max.1724dn@gmail.com>
2023-11-16 23:47:12 +0900

    PostView

--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -1,6 +1,13 @@
+import type { User } from "@clerk/nextjs/server";
+import { clerkClient } from "@clerk/nextjs/server";
+import { TRPCError } from "@trpc/server";
 import { z } from "zod";
 
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
+
+const filterUserForClient = (user: User) => {
+  return { id: user.id, imageUrl: user.imageUrl, username: user.username };
+};
 
 export const postsRouter = createTRPCRouter({
   // create: publicProcedure
@@ -19,7 +26,29 @@ export const postsRouter = createTRPCRouter({
   //   });
   // }),
 
-  getAll: publicProcedure.query(({ ctx }) => {
-    return ctx.db.post.findMany();
+  getAll: publicProcedure.query(async ({ ctx }) => {
+    const posts = await ctx.db.post.findMany({
+      take: 100,
+    });
+
+    const users = await clerkClient.users.getUserList({
+      userId: posts.map((post) => post.authorId),
+      limit: 100,
+    });
+
+    users.map(filterUserForClient);
+
+    return posts.map((post) => {
+      const author = users.find((user) => user.id === post.authorId);
+      if (!author || author === undefined)
+        throw new TRPCError({
+          code: "INTERNAL_SERVER_ERROR",
+          message: "Author for posts not found",
+        });
+      return {
+        post,
+        author,
+      };
+    });
   }),
 });
